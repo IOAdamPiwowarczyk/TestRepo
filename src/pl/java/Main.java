7e2284d14b363dec955c938ad43a4cc8dbd230e6
AdamPiwowarczyk <lothur666@gmail.com>
2019-10-20 23:31:15 +0200

    main project 2

--- src/pl/java/Main.java
+++ src/pl/java/Main.java
@@ -3,7 +3,7 @@ package pl.java;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("zmieniony main");
+		System.out.println("zmieniony main w 2 projekcie");
 
 	}
 
